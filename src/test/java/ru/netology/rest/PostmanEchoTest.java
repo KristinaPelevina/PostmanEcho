8b8b08a6b0daf29f41f8e935d7b0e7d803802dac
Kristina Pelevina <christya_13.07@mail.ru>
2020-09-12 20:32:22 +0600

    Update PostmanEchoTest.java

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -3,7 +3,6 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
 
 class PostmanEchoTest {
     @Test
